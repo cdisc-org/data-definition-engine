53ca4c893bc92403804bc4a19455c28920d1eb37
dostiep <111854855+dostiep@users.noreply.github.com>
2026-04-13 20:34:47 +0200

    Update

--- src/define-xml/create_define_json.py
+++ src/define-xml/create_define_json.py
@@ -112,6 +112,7 @@ class USDMDefineJSONProcessor:
             'whereClauses': [],
             'codeLists': [],
             'methods': [],
+            'comments': [],
             'standards': [],
             'annotatedCRF': [],
             'concepts': [],
